fe826442ac49819055ae3f9204228c9551e3d2aa
alorenzi10 <a.lorenzi10@studenti.unibg.it>
2023-04-24 12:04:15 +0200

    Dichiarazione tessere da gioco

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,24 @@
 package main;
 
 public class Main {
+	public static void main(String[] args){
+		
+		//prova creazione tiles e mischia
+		Tiles.Inizializza();
+		Tiles.Mischia();
+	}
 
 	
 	// prova alessandro
 	//prova francesca
 	//prova giulia
+	//prova francesca 2
+// prova luca
+
+	
+	
+	//prova giulia 2
+
+	
+	
 }
