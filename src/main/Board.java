7a89a2e2f3eb23a32e247537eb68bcbf9bbe3018
alorenzi10 <a.lorenzi10@studenti.unibg.it>
2023-05-04 15:02:03 +0200

    Aggiunta tessere su board
    funzionante anche in base a n giocatori (metodo un po meccanico)

--- src/main/Board.java
+++ src/main/Board.java
@@ -1,113 +1,108 @@
 package main;
 
+import java.util.Random;
+
 public class Board{
 	
 	private static int counter=0;
-	private static Tile[] riga1= new Tile[2];
-	private static Tile[] riga2= new Tile[3];
-	private static Tile[] riga3= new Tile[5];
-	private static Tile[] riga4= new Tile[8];
-	private static Tile[] riga5= new Tile[9];
-	private static Tile[] riga6= new Tile[8];
-	private static Tile[] riga7= new Tile[5];
-	private static Tile[] riga8= new Tile[3];
-	private static Tile[] riga9= new Tile[2];
+	private static int righe=11;
+	private static int colonne=11;
 	
-	public static void Board(int giocatori){
-		counter=0;
-		Tiles.Inizializza();
-		Tiles.Mischia();
+	public static Tile[][] board=new Tile[righe][colonne];
+	public static Tile[] avanzate=new Tile[10]; //preso 10 perché non penso ne avanzino di più
+	
+	public static void Board(int ngiocatori){
 		
-		for(int i=0; i<riga1.length; i++) {
-			riga1[i]=Tiles.tiles[counter];
-			counter++;
+		Tiles.Inizializza(); //if primo giro
+		Tiles.Mischia();
+		Board.BoardSetUp(ngiocatori);
+		Board.StampaBoard();
+	} 
+	 
+	
+	public static void BoardSetUp(int ngiocatori) {
+		
+		int i=0;
+		for(int riga=0; riga<righe; riga++) {  //controlla pezzi avanzati e resetta tutto a null
+			for(int colonna=0; colonna<colonne; colonna++) {
+				if(board[riga][colonna]!=null) {
+					avanzate[i]=board[riga][colonna];
+					i++;
+				}
+				board[riga][colonna]=null;
+			}
 		}
-		for(int i=0; i<riga2.length; i++) {
-			riga2[i]=Tiles.tiles[counter];
-			counter++;
+		
+		if(i>0) { //se avanzano pezzi li rimetto nelle tiles e mischio
+			counter=counter-i;
+			for(int x=counter; x<counter; x++) {
+				
+				Tiles.tiles[x]=avanzate[i-1];
+				i--;
+			}
+			Random rand=new Random();
+			
+			for(int x=counter; x<132;x++) {
+				int r=x+rand.nextInt(132-counter);
+				Tile temp=Tiles.tiles[r];
+				Tiles.tiles[r]=Tiles.tiles[x];
+				Tiles.tiles[x]=temp;
+			}
 		}
-		for(int i=0; i<riga3.length; i++) {
-			riga3[i]=Tiles.tiles[counter];
-			counter++;
+		
+		for (int riga=3; riga<8; riga++) { //aggiungo le tiles alla board
+			for(int colonna=3; colonna<8; colonna++) {
+				if(!((colonna==3 || colonna==7) && (riga==3 || riga==7))){
+				
+					board[riga][colonna]=Tiles.tiles[counter];
+					counter++;
+				}	
+			}
 		}
-		for(int i=0; i<riga4.length; i++) {
-			riga4[i]=Tiles.tiles[counter];
-			counter++;
-		}
-		for(int i=0; i<riga5.length; i++) {
-			riga5[i]=Tiles.tiles[counter];
-			counter++;
-		}
-		for(int i=0; i<riga6.length; i++) {
-			riga6[i]=Tiles.tiles[counter];
-			counter++;
-		}
-		for(int i=0; i<riga7.length; i++) {
-			riga7[i]=Tiles.tiles[counter];
-			counter++;
-		}
-		for(int i=0; i<riga8.length; i++) {
-			riga8[i]=Tiles.tiles[counter];
-			counter++;
-		}
-		for(int i=0; i<riga9.length; i++) {
-			riga9[i]=Tiles.tiles[counter];
-			counter++;
+		board[2][4]=Tiles.tiles[counter];
+		board[2][5]=Tiles.tiles[++counter];
+		board[4][8]=Tiles.tiles[++counter];
+		board[5][2]=Tiles.tiles[++counter];
+		board[5][8]=Tiles.tiles[++counter];
+		board[6][2]=Tiles.tiles[++counter];
+		board[8][5]=Tiles.tiles[++counter];
+		board[8][6]=Tiles.tiles[++counter];
+		
+		if(ngiocatori>2) {
+			board[1][4]=Tiles.tiles[counter];
+			board[3][3]=Tiles.tiles[++counter];
+			board[3][7]=Tiles.tiles[++counter];
+			board[4][9]=Tiles.tiles[counter];
+			board[6][1]=Tiles.tiles[counter];
+			board[7][3]=Tiles.tiles[++counter];
+			board[7][7]=Tiles.tiles[++counter];
+			board[9][6]=Tiles.tiles[++counter];
+			if(ngiocatori>3) {
+				board[1][5]=Tiles.tiles[counter];
+				board[2][6]=Tiles.tiles[counter];
+				board[4][2]=Tiles.tiles[counter];
+				board[5][1]=Tiles.tiles[counter];
+				board[5][9]=Tiles.tiles[counter];
+				board[6][8]=Tiles.tiles[counter];
+				board[8][4]=Tiles.tiles[counter];
+				board[9][5]=Tiles.tiles[counter];
+			}
 		}
 	}
 	
 	public static void StampaBoard() {
 		
-		for(int i=0; i<riga1.length; i++) {
-			System.out.print(riga1[i]+" ");
-			
+		for(int riga=0; riga<righe; riga++) {
+			System.out.println(" ");
+			for(int colonna=0; colonna<colonne; colonna++) {
+				if(board[riga][colonna]==null) {
+					System.out.print("     ");
+				}
+				else {
+				System.out.print(board[riga][colonna].getColor()+" ");
+				}
+			}
 		}
-		System.out.println(" ");
-		for(int i=0; i<riga2.length; i++) {
-			System.out.print(riga2[i]+" ");
-			
-		}
-		System.out.println(" ");
-		for(int i=0; i<riga3.length; i++) {
-			System.out.print(riga3[i]+" ");
-			
-		}
-		System.out.println(" ");
-		for(int i=0; i<riga4.length; i++) {
-			System.out.print(riga4[i]+" ");
-			
-		}
-		System.out.println(" ");
-		for(int i=0; i<riga5.length; i++) {
-			System.out.print(riga5[i]+" ");
-			
-		}
-		System.out.println(" ");
-		for(int i=0; i<riga6.length; i++) {
-			System.out.print(riga6[i]+" ");
-			
-		}
-		System.out.println(" ");
-		for(int i=0; i<riga7.length; i++) {
-			System.out.print(riga7[i]+" ");
-			
-		}
-		System.out.println(" ");
-		for(int i=0; i<riga8.length; i++) {
-			System.out.print(riga8[i]+" ");
-			
-		}
-		System.out.println(" ");
-		for(int i=0; i<riga9.length; i++) {
-			System.out.print(riga9[i]+" ");
-			
-		}
-		System.out.print(counter);
 	}
-	
-	public static void Prendibili() {
-		
-	}
-	
 }
 
